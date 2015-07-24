e62aa58b53b5c3b3a87d64bb97034f927faca573
setoyama-precena <masato.setoyama@precena.com>
2015-07-24 15:59:28 +0900

    can add some class to form-wrap span tag

--- lib/formtastic-bootstrap/inputs/base/wrapping.rb
+++ lib/formtastic-bootstrap/inputs/base/wrapping.rb
@@ -6,23 +6,27 @@ module FormtasticBootstrap
         include Formtastic::Inputs::Base::Wrapping
 
         def bootstrap_wrapping(&block)
+
+          form_wrap_class = 'form-wrapper'
+          form_wrap_class = form_wrap_class + ' ' + options[:form_wrap_class] if options[:form_wrap_class]
+
           form_group_wrapping do
             label_html <<
-            template.content_tag(:span, :class => 'form-wrapper') do
-              input_content(&block) <<
-              hint_html(:block) <<
-              error_html(:block)
-            end
+                template.content_tag(:span, :class => form_wrap_class) do
+                  input_content(&block) <<
+                      hint_html(:block) <<
+                      error_html(:block)
+                end
           end
         end
 
         def input_content(&block)
           content = [
-            add_on_content(options[:prepend]),
-            options[:prepend_content],
-            yield,
-            add_on_content(options[:append]),
-            options[:append_content]
+              add_on_content(options[:prepend]),
+              options[:prepend_content],
+              yield,
+              add_on_content(options[:append]),
+              options[:append_content]
           ].compact.join("\n").html_safe
 
           if prepended_or_appended?(options)
@@ -43,8 +47,8 @@ module FormtasticBootstrap
 
         def form_group_wrapping(&block)
           template.content_tag(:div,
-            template.capture(&block).html_safe,
-            wrapper_html_options
+                               template.capture(&block).html_safe,
+                               wrapper_html_options
           )
         end
 
